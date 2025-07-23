ea6677bdbba6150f6a7ef0ffbfc0829b395001cf
ClarkAltius <ilhwana@gmail.com>
2025-07-23 14:25:54 +0900

    second commit

---
+++ src/ch01_variable_operator/Add.java
@@ -0,0 +1,25 @@
+package ch01_variable_operator;
+
+public class Add {
+    public static void main(String[] args) {
+        int a ; //int literally means integer. this defines the variable a as an integer which I can use later.
+        a = 3 ; //here I assign the declared variable a to the number 3.
+        int b ;
+        b = 66 ;
+        int result ;
+
+        result = 2*a + 3*b;
+        System.out.printf("3 times 2 plus 3 times 66 is ");
+        System.out.println(result);
+        String message ; //this is literally a string of characters. This is also a declaration of a variable.
+        message = a + " times 2 plus 3 times " + b + " is " ;
+        System.out.printf(message);
+        System.out.println(result);
+        //same result, different approaches. but this now will reflect the changes in variables automatically.
+        int x = 55 ;
+        int y = 44 ;
+        String new_message ;
+        new_message = x + " 곱하기 " + y + " 는 " + x*y + " 입니다!";
+        System.out.println(new_message);
+    }
+}
--- src/ch01_variable_operator/MyPrint.java
+++ src/ch01_variable_operator/MyPrint.java
@@ -1,4 +1,22 @@
 package ch01_variable_operator;
 
+/*
+everything inside is a comment. got it?
+lol
+*/
+
 public class MyPrint {
+public static void main(String[] args) {
+    System.out.printf("why is python so much simpler? ");
+//        System.out.println("");
+//        System.out.println("in python all it needs is print(\"hello world\")");
+    System.out.printf("대한");
+    System.out.printf(" 민국");
+    System.out.printf(" 화이팅");
+    System.out.println();
+    System.out.println("\"Glorious\" Lance \'Charge\'");
+    System.out.println("Duke\tBaron\tPaladin\t");
+    System.out.println("Armand\t \n Chilfroix\tHenri\t");
+    // This is a comment.
+}
 }
