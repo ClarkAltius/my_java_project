b40334132f60cf1cf0f44998170447e740fd58c5
ClarkAltius <ilhwana@gmail.com>
2025-07-23 14:28:09 +0900

    second commit

--- src/ch01_variable_operator/Add.java
+++ src/ch01_variable_operator/Add.java
@@ -9,11 +9,11 @@ public class Add {
         int result ;
 
         result = 2*a + 3*b;
-        System.out.printf("3 times 2 plus 3 times 66 is ");
+        System.out.print("3 times 2 plus 3 times 66 is ");
         System.out.println(result);
         String message ; //this is literally a string of characters. This is also a declaration of a variable.
         message = a + " times 2 plus 3 times " + b + " is " ;
-        System.out.printf(message);
+        System.out.print(message);
         System.out.println(result);
         //same result, different approaches. but this now will reflect the changes in variables automatically.
         int x = 55 ;
